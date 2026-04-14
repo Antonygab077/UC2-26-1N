cb106ac8393ea747cf595e4f95a79cf4bc8c51fd
Antonygab077 <antonygabriel10oliveiragarcia@gmail.com>
2026-04-13 21:47:24 -0300

    Add files via upload

---
+++ 13-04.js
@@ -0,0 +1,28 @@
+const ask = require(`readline-sync`)
+
+/*
+let msg1 = `CAMINHAO CAI NOS TRILHOS E AFETA PASSAGEIROS`;
+let msg2 = msg1.toLowerCase()
+console.log(`Antes: ${msg1}, Depois: ${msg2}`)
+
+const opniao = ask.question(`Qual sua opniao sobre o acidente? `);
+const maiuscula = opniao.toUpperCase();
+console.log(`Antes: ${opniao}\nDepois: ${maiuscula})
+
+let msg3 = `     Trem parado por causa de um caminhao nos trilhos     `;
+let noSpace = msg3.trim();
+console.log(`Com espaco: ${msg3}, Caracteres: ${msg3.length}\nSem espaco: ${noSpace}, Caracteres: ${noSpace.length}`);
+
+let fraseUser = ask.question(`Digite uma frase: `)
+let verification = ask.question(`Digite uma palavra que deseja verificar: `)
+console.log(`Frase: ${fraseUser}\n
+Verificacao: ${verification}\n
+${verification} aparece em ${fraseUser}? ${fraseUser.includes(verification)}
+`)
+
+const descrition = ask.question(`Descreva o impacto do acidente: `)
+const wordSubst = ask.question(`Descreva a palavra a ser substituida: `)
+const wordNow = ask.question(`Descreva a palavra que ira substituir: `)
+const result = descrition.replaceAll(wordSubst, wordNow)
+console.log(result)
+*/
