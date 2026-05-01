70aa443dd23c663300e60369d6456f6394622a1b
Antonygab077 <antonygabriel10oliveiragarcia@gmail.com>
2026-05-01 02:18:18 -0300

    trabalhoresult.js

--- trabalhoresult.js
+++ trabalhoresult.js
@@ -1,31 +1,38 @@
-function criarUsuario(nome, idade, cidade, hobbies){
-    return [nome, idade, cidade, hobbies];
+const ask = require(`readline-sync`)
+
+function criarUsuario(name1, age, city, hobbies){
+    return [name1, age, city, hobbies]
 }
 
 function formatarHobbies(hobbies){
-    return hobbies.join(", ");
+    return hobbies.join(", ")
 }
 
 function gerarDescricao(usuario){
-    return `${usuario[0]}, tem ${usuario[1]} anos, mora em ${usuario[2]} e gosta de ${formatarHobbies(usuario[3])}`
+    return `${usuario[0]} tem ${usuario[1]} anos, mora em ${usuario[2]} e gosta de: ${formatarHobbies(usuario[3])}`
 }
 
 function adicionarUsuario(usuarios, usuario){
-    let usuarioAtualizado = [...usuarios, usuario]
-    return usuarioAtualizado
+    let userAtualizado = [...usuarios, usuario]
+    return userAtualizado
 }
 
 function gerarRelatorio(usuarios){
-    return `Total de usuarios: ${usuarios.length}`
+    return usuarios.length
 }
 
-let = usuarios = [] 
+let questionName = ask.question(`Insira seu nome: `)
+let questionAge = Number(ask.question(`Insira sua idade: `))
+let questionCity = ask.question(`Insira sua cidade: `)
+let questionHobbie1 = ask.question(`Insira seu primeiro hobbie: `)
+let questionHobbie2 = ask.question(`Insira seu segundo hobbie: `)
+let questionHobbie3 = ask.question(`Insira seu terceiro hobbie: `)
+let user1 = criarUsuario(questionName, questionAge, questionCity, [questionHobbie1, questionHobbie2, questionHobbie3])
 
-let user1 = criarUsuario(`Antony`, 16, `Sapucaia do Sul`, [`Musica`, `Guitarra`, `Jogar`]);
-let user2 = criarUsuario(`Erick`, 20, `Sao leo`, [`Correr`, `Dar tiro`, `Morrer`])
+let usuarios = []
 
 console.log(user1)
 console.log(formatarHobbies(user1[3]))
 console.log(gerarDescricao(user1))
-usuarios = adicionarUsuario(user1, user2)
+usuarios = adicionarUsuario(usuarios, user1)
 console.log(gerarRelatorio(usuarios))
