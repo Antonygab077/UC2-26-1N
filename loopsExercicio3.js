86a89309333505c187bc1481f07b2d421ab25fd3
Antonygab077 <antonygabriel10oliveiragarcia@gmail.com>
2026-05-12 21:44:17 -0300

    Add files via upload

---
+++ loopsExercicio3.js
@@ -0,0 +1,9 @@
+let myArray = [`Oi`, `sumido`, `tudo`, `bem?`, `Saudades`, `kk`];
+
+let fraseInteria = ``
+
+for(let palavra of myArray) {
+    fraseInteria = fraseInteria + ` ` + palavra
+}
+
+console.log(fraseInteria)
