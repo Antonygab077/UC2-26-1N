ec7b39009bbde9a1cd99b6df99724f0a0f974478
Antony Gabriel de Oliveira Garcia <antonygabriel10oliveiragarcia@gmail.com>
2026-05-19 21:32:58 -0300

    Add files via upload

---
+++ objetosExercicio2.js
@@ -0,0 +1,8 @@
+const person = {
+    name: `Antony`,
+    age: 16,
+    favMusicGenre: `Rock` 
+}
+
+console.clear()
+console.log(`This person's name is ${person.name}. He is ${person.age} years old, and his favorite music genre is: ${person.favMusicGenre}`)
